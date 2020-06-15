fe4b3afc8286f1bf338d2119c87a0bf542333413
NekoCarreraDesigns <denvermaas@gmail.com>
2020-06-15 15:57:52 -0600

    created table for the console

--- js/employee.js
+++ js/employee.js
@@ -1,7 +1,8 @@
+//declare dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require(console.table);
-
+//creata database connection
 const connection = mysql.createConnection({
     host: "employeeHost",
     port: 3306,
@@ -9,18 +10,34 @@ const connection = mysql.createConnection({
     password: "8Limb$Thai1978",
     database: "employee_DB"
 })
-
+//connect to database
 connection.connect(function (err) {
     if (err) throw (err);
     console.log("Shalom brother!")
     start();
 })
+// created console.table to print to terminal
+// const table = cTable.getTable([{
+//     id: "",
+//     firstName: "",
+//     lastName: "",
+//     title: "",
+//     department: "",
+//     manager: "",
 
+// },
+// ])
+
+// console.log(table)
+
+
+
+//initial function prompt for app with list of options
 function start() {
     inquirer.prompt({
         name: "intro",
         type: "list",
-        message: "What would you lie to do?",
+        message: "What would you like to do?",
         choices: [
             "View All Employees",
             "View All Employees by Department",
